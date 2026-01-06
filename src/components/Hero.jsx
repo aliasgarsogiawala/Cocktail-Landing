55f16928291c03def38b6c1724f9404bdb05e116
Aliasgar Sogiawala <vertexxinfotech18@gmail.com>
2026-01-06 22:11:03 +0530

    Hero section started

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,13 @@ const Hero = () => {
     return (
         <>
         <section id="hero" className="noisy">
+            <h1 className="title">MOJITO</h1>
+
+            <img src="/images/hero-left-leaf.png" alt="Left leaf"
+            className="left-leaf" />
+            <img src="/images/hero-right-leaf.png" alt="Right leaf"
+                 className="right-leaf" />
+
         </section></>
     )
 }
