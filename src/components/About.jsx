fc7a7dda7e7f63adea261b72560cdb990ed26cc9
Aliasgar Sogiawala <vertexxinfotech18@gmail.com>
2026-01-07 12:30:05 +0530

    About section started'

---
+++ src/components/About.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div id="about">
+            <div className="mb-16 md:px-0 px-5">
+                <div className="content">
+                    <div className="md:col-span-8">
+                        <p className="badge">Best Cocktails</p>
+                        <h2>
+                            Where every details matters <span
+                            className="text-white">- </span>
+                            from muddle to garnish
+                        </h2>
+                    </div>
+
+                    <div className="sub-content">
+                        <p>
+                            Every cocktail we serve is a reflection of our obsession with detail — from the first muddle to the final garnish. That care is what turns a simple drink into something truly memorable.
+                        </p>
+
+                        <div>
+                            <p className="md:text-3xl text-xl font-bold">
+                                <span>4.7</span>/5
+                            </p>
+                            <p className="text-sm text-white-100">
+                                More than +15000 customers
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div clasName="top-grid">
+                <div className="md:col-span-3">
+                    <div className="noisy"/>
+                    <img src="/images/abt1.png" alt="grid-img-1" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
